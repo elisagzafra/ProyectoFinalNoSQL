a0f8e2b4693b1a984b1cf9b1b1b048b793bcb20c
elisagzafra <elisagz@icloud.com>
2023-05-24 14:57:41 -0600

    api

--- api.py
+++ api.py
@@ -5,15 +5,14 @@ url = 'https://rickandmortyapi.com/api/character/'
 response = requests.get(url)
 data = response.json()
 
-all_characters = []
+all_characters = data['results']  # Agregar los primeros personajes obtenidos
 
-while data['info']['next']:
+while 'next' in data['info'] and data['info']['next'] is not None:  # Verificar si 'next' existe y no es None
     response = requests.get(data['info']['next'])
     data = response.json()
-    for result in data['results']:
-        all_characters.append(result)
+    all_characters.extend(data['results'])  # Agregar los personajes de la página actual
 
-with open('rick_and_morty_characters.json', 'w', encoding='utf-8') as f:
+with open('rick_and_morty_characters1.json', 'w', encoding='utf-8') as f:
     for character in all_characters:
         f.write(json.dumps(character, ensure_ascii=False, indent=4))
         f.write('\n')
